d8e4e1b4a070a812ae81993bae2e632486fb59c5
MuskanBansal3 <40180269+MuskanBansal3@users.noreply.github.com>
2020-02-17 01:51:40 +0530

    Add files via upload

---
+++ LayeredArchitecture/src/com/ibm/test/dao/DaoInterface.java
@@ -0,0 +1,15 @@
+package com.ibm.test.dao;
+
+import java.util.HashMap;
+
+import java.util.Map;
+
+import com.ibm.test.bean.Person;
+
+public interface DaoInterface {
+	
+	
+	void storeIntoMap(Person person);
+	Map<Integer,Person> displayPersons();
+
+}
